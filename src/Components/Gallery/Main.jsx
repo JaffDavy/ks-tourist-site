14c5288c771077b1e4f30d5cc05d2aac29bf1cc8
jaff davy <jaffdavy100@gmail.com>
2025-12-04 13:43:46 +0100

    feature worked on the Gallery page

---
+++ src/Components/Gallery/Main.jsx
@@ -0,0 +1,204 @@
+export default function Main() {
+  return (
+    <div>
+      <div className="flex flex-row px-20 my-5 gap-5 justify-center">
+        <div className="flex flex-col gap-3 w-fit">
+          <div>
+            <img
+              src="/Image Placeholder (4).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+          <div>
+            <img
+              src="/Image Placeholder (5).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+        </div>
+        <div>
+          <img
+            src="/Image Placeholder (6).png"
+            alt=""
+            className="w-full h-full"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <div>
+            <img
+              src="/Image Placeholder (7).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+          <div>
+            <img
+              src="/Image Placeholder (8).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div>
+            <img
+              src="/Image Placeholder (9).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+          <div className="flex flex-row gap-4">
+            <div>
+              <img
+                src="/Image Placeholder (10).png"
+                alt=""
+                className="w-full h-full"
+              />
+            </div>
+            <div>
+              <img
+                src="/Image Placeholder (11).png"
+                alt=""
+                className="w-full h-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row px-35 my-5 gap-5 flex-wrap justify-center">
+        <div className="flex flex-col gap-2">
+          <div>
+            <img
+              src="/Image Placeholder (12).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+          <div className="flex flex-row gap-4">
+            <div>
+              <img
+                src="/Image Placeholder (13).png"
+                alt=""
+                className="w-full h-full"
+              />
+            </div>
+            <div>
+              <img
+                src="/Image Placeholder (14).png"
+                alt=""
+                className="w-full h-full"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <div>
+            <img
+              src="/Image Placeholder (15).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+          <div>
+            <img
+              src="/Image Placeholder (16).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+        </div>
+        <div>
+          <img
+            src="/Image Placeholder (17).png"
+            alt=""
+            className="w-full h-full"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <div>
+            <img
+              src="/Image Placeholder (18).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+          <div>
+            <img
+              src="/Image Placeholder (19).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row px-20 my-5 gap-5 justify-center">
+        <div className="flex flex-col gap-3 w-fit">
+          <div>
+            <img
+              src="/Image Placeholder (20).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+          <div>
+            <img
+              src="/Image Placeholder (21).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+        </div>
+        <div>
+          <img
+            src="/Image Placeholder (22).png"
+            alt=""
+            className="w-full h-full"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <div>
+            <img
+              src="/Image Placeholder (23).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+          <div>
+            <img
+              src="/Image Placeholder (24).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div>
+            <img
+              src="/Image Placeholder (25).png"
+              alt=""
+              className="w-full h-full"
+            />
+          </div>
+          <div className="flex flex-row gap-4">
+            <div>
+              <img
+                src="/Image Placeholder (26).png"
+                alt=""
+                className="w-full h-full"
+              />
+            </div>
+            <div>
+              <img
+                src="/Image Placeholder (27).png"
+                alt=""
+                className="w-full h-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
